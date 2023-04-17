fa37fe620e2d2a7f3079a60729382719ea3f8e29
2Bid <salenbieraxel@gmail.com>
2023-04-17 15:33:56 +0200

    add react Route

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
                <Route index path='/' element={<Log />}></Route>
                <Route path="/profil/:id" element={<Dashboard/>}></Route>
                <Route path="*" element={<P404/>}></Route>
+               {/* Github pages required */}
+               <Route index path='/AxelSalenbier_12_24042022/' element={<Log />}></Route>
         </Routes>
     </div>
   );
